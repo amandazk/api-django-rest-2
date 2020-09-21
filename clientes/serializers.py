fe8ce8f3ae4bcc9451cfe9c5d2a4bd65d4418770
amandazk <zacharukaz@gmail.com>
2020-09-21 11:24:57 -0300

    Importando validações

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,7 +8,7 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf':"O CPF deve ser válido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':"O Nome deve conter apenas letras"}) 
         if not rg_valido(data['rg']):
