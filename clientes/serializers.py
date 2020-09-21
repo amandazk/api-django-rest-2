98919ed1cfb3e013a1d9ad5aa0be841dfc9c836e
amandazk <zacharukaz@gmail.com>
2020-09-21 10:08:21 -0300

    Validando todos os campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -9,3 +9,15 @@ class ClienteSerializer(serializers.ModelSerializer):
         if len(cpf) != 11:
             raise serializers.ValidationError("O CPF deve ter 11 dígitos")
         return cpf
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("O Nome deve conter apenas letras")
+        return nome
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 dígitos")
+        return rg
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O Celular deve ter no mínimo 11 dígitos")
+        return celular
